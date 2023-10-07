b9c08df27c9ed172cfd2d486b7ed488f9acc3d19
Admin1 <a89086829981@gmail.com>
2023-10-07 15:14:28 +0300

    Added viewBinding

--- app/src/main/java/com/example/composition3/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition3/presentation/GameFinishedFragment.kt
@@ -6,14 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.composition3.R
+import com.example.composition3.databinding.FragmentGameBinding
+import com.example.composition3.databinding.FragmentGameFinishedBinding
 
 class GameFinishedFragment: Fragment() {
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return  inflater.inflate(R.layout.fragment_game_finished, container, false)
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
